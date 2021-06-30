43efea5c3b664f7b26cd2fec81e4862d958b050b
LazyGeniusMan <LazyGeniusMan@gmail.com>
2021-07-01 03:16:49 +0700

    Add a function to get character talent data

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 const db = require('genshin-db');
+const csvFormat = require('@fast-csv/format');
 
 //Get data from genshin-db
 function getData(type, query, level, ascended) {
     switch (type) {
         case 'character':
-            return db.character(query, { matchCategories: true });
+            return db.characters(query, { matchCategories: true });
             break;
         case 'character-stats':
-            return db.character(query).stats(level, ascended);
+            return db.characters(query).stats(level, ascended);
+            break;
+        case 'character-talents':
+            return db.talents(query);
             break;    
         case 'weapon':
-            return db.weapon(query, { matchCategories: true });
+            return db.weapons(query, { matchCategories: true });
             break;
         case 'weapon-stats':
-            return db.weapon(query).stats(level, ascended);
+            return db.weapons(query).stats(level, ascended);
             break;
         default:
             break;
